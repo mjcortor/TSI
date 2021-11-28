95707130162c58699e34ae68b0c851404128d929
Antonio <guerrero.aguilar.antoniojose@gmail.com>
2021-11-28 12:42:58 +0100

    Valoraciones citas

--- municitic/models/tipotrabajador.py
+++ municitic/models/tipotrabajador.py
@@ -13,3 +13,9 @@ class tipotrabajador(models.Model):
     
     trabajadores_ids = fields.One2many("municitic.trabajadores","tipotrabajador_ids","Trabajador confirmado")
     comunicados_ids = fields.One2many("municitic.comunicados","tipotrabajador_ids","Comunicado confirmado")
+    nComunicados = fields.Integer(compute='_nComunicados',string='Comunicados total',store=True)
+    
+    @api.depends('comunicados_ids')
+    def _nComunicados(self): 
+        for record in self:                            
+            record.nComunicados = len(record.comunicados_ids)
