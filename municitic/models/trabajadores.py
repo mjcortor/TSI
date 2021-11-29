4a97f36e1cde279d1dc34a9a876a5c74f407adc7
Antonio <guerrero.aguilar.antoniojose@gmail.com>
2021-11-29 11:32:52 +0100

    Vista pivot trabajadores e incidencias

--- municitic/models/trabajadores.py
+++ municitic/models/trabajadores.py
@@ -10,9 +10,14 @@ class Trabajadores(models.Model):
     usuario = fields.Char(string="Usuario", required=True, help="usuario")
     direccion = fields.Text()
     photo=fields.Binary('Photo')
+    nIncidencias = fields.Integer(compute='_nIncidencias',string='Incidencias total',store=True)
     
     citas_ids = fields.One2many("municitic.citas","trabajadores_ids","Cita confirmada")
     incidencias_ids = fields.Many2many("municitic.incidencias",string="Incidencias confirmados")
     tipotrabajador_ids = fields.Many2one("municitic.tipotrabajador",string="Tipo Trabajador confirmado")
     cursos_ids = fields.Many2many("municitic.cursos",string="Cursos confirmados")
 
+    @api.depends('incidencias_ids')
+    def _nIncidencias(self): 
+        for record in self:                            
+            record.nIncidencias = len(record.incidencias_ids)
