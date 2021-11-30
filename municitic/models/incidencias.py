4d29009bbfe6b2ccedae1bd0e92dc1daca68b073
Antonio <guerrero.aguilar.antoniojose@gmail.com>
2021-11-30 13:17:13 +0100

    Vista pivot Incidencias

--- municitic/models/incidencias.py
+++ municitic/models/incidencias.py
@@ -8,12 +8,19 @@ class Incidencias(models.Model):
     name = fields.Char(string="Nombre", required=True, help="nombre de la incidencia")
     description = fields.Text()
     ubicacion = fields.Text()
-    start = fields.Datetime('Fecha y Hora',required=True, autodate = True)
-    end = fields.Datetime('Fecha y Hora',required=True, autodate = True)   
-    activityType = fields.Selection([('urbanistico','Urbanístico'),#tipo incidencia renombrar
+    start = fields.Datetime('Comienza',required=True, autodate = True)
+    end = fields.Datetime('Finaliza',required=True, autodate = True)   
+    activityType = fields.Selection([('urbanistico','Urbanístico'),
                                     ('emergencia','Emergencia'),
                                     ('trafico','Tráfico'),],
                                     'Tipo de incidencia')
+    nTrabajadores = fields.Integer(compute='_nTrabajadores',string='Trabajadores total',store=True)
     valoraciones_ids = fields.Many2one("municitic.valoraciones",string="Valoraciones confirmadas")
     trabajadores_ids = fields.Many2many("municitic.trabajadores",string="Trabajadores confirmados")
     usuarios_ids = fields.Many2one("municitic.usuarios",string="Usuarios confirmados")
+    
+    
+    @api.depends('trabajadores_ids')
+    def _nTrabajadores(self): 
+        for record in self:                            
+            record.nTrabajadores = len(record.trabajadores_ids)
--- municitic/models/trabajadores.py
+++ municitic/models/trabajadores.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class Trabajadores(models.Model):
     _name = 'municitic.trabajadores'
     _description = 'Municitic Trabajadores'
-    nombre = fields.Char(string="Nombre", required=True, help="nombre completo")
+    name = fields.Char(string="Nombre", required=True, help="nombre completo")
     dni = fields.Char(string="DNI", required=True, help="dni")
     usuario = fields.Char(string="Usuario", required=True, help="usuario")
     direccion = fields.Text()
