31c3ddcdc59be79fa610208100467ed27b5bb001
Antonio <guerrero.aguilar.antoniojose@gmail.com>
2021-11-30 13:35:18 +0100

    Cambio en los atributos de nombre

--- municitic/models/usuarios.py
+++ municitic/models/usuarios.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class Usuarios(models.Model):
     _name = 'municitic.usuarios'
     _description = 'Municitic Usuarios'
-    nombre = fields.Char(string="Nombre", required=True, help="nombre completo")
+    name = fields.Char(string="Nombre", required=True, help="nombre completo")
     dni = fields.Char(string="DNI", required=True, help="dni")
     usuario = fields.Char(string="Usuario", required=True, help="usuario")
     direccion = fields.Text()
