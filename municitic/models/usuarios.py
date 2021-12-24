870435ea92463a5985bb9164f7c76d6a702d8f51
Antonio <guerrero.aguilar.antoniojose@gmail.com>
2021-12-24 13:33:10 +0100

    Validación del dni

--- municitic/models/usuarios.py
+++ municitic/models/usuarios.py
@@ -14,3 +14,15 @@ class Usuarios(models.Model):
     citas_ids = fields.Many2many("municitic.citas",string="Cita confirmada")
     incidencias_ids = fields.One2many("municitic.incidencias","usuarios_ids","Incidencia confirmada")
     comunicados_ids = fields.Many2many("municitic.comunicados",string="Comunicado confirmado")
+
+    @api.constrains('dni')
+    def _check_dni(self):
+        if len(self.dni) != 9:
+            raise models.ValidationError('El dni debe tener una longitud de 9')
+        elif len(self.dni) == 9:
+            letra = self.dni[8]
+            subs = self.dni[0:7]
+            if(letra.isdigit()):
+                raise models.ValidationError('El dni debe tener una letra al final')
+            elif(subs.isdigit() == False):
+                raise models.ValidationError('El dni no debe contener números en los 8 primeros carácteres')
